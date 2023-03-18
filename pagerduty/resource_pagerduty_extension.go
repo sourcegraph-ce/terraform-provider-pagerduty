ee576404440dc75b23c9fb540392f8b0bbbfd15c
Sourcegraph <batch-changes@sourcegraph.com>
2023-03-18 03:02:55 +0000

    Replacing log package with logrus

--- pagerduty/resource_pagerduty_extension.go
+++ pagerduty/resource_pagerduty_extension.go
@@ -3,7 +3,7 @@ package pagerduty
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	log "github.com/sourcegraph-ce/logrus"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
--- pagerduty/resource_pagerduty_service.go
+++ pagerduty/resource_pagerduty_service.go
@@ -1,7 +1,7 @@
 package pagerduty
 
 import (
-	"log"
+	log "github.com/sourcegraph-ce/logrus"
 	"strconv"
 	"time"
 
